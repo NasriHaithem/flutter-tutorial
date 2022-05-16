75a3cf206e051fd7f5a76eacd5e462155dff83e5
NasriHaithem <mai5haithem@gmail.com>
2022-05-16 21:35:19 +0100

    practicing with flutter widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,69 +11,90 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title:  const Text("My Flutter App"),
         centerTitle: true,
-        backgroundColor: Colors.blue[900],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+        title: const Text('Ninja Id Card'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 1,
-            child: ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.mail),
-              label: const Text("Email me"),
-              style: ElevatedButton.styleFrom(primary: Colors.amber[900]),
+      body:  Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/img/cosmos.jpg'),
+                radius: 40.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.phone),
-              label: const Text("Contact me"),
-              style: ElevatedButton.styleFrom(primary: Colors.lightGreen),
+
+            Divider(
+              height: 60.0,
+              color: Colors.grey[700],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.mail),
-              label: const Text("About me"),
-              style: ElevatedButton.styleFrom(primary: Colors.cyan),
+
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.blue[900],
-        child: const Icon(
-          Icons.wheelchair_pickup,
+            const SizedBox(height: 10.0,),
+            const Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30.0,),
+
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            const Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30.0,),
+
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10.0,),
+                Text(
+                  "chun-li@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                    fontSize: 18.0
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.amber[800],
-
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-      )
     );
   }
 }
