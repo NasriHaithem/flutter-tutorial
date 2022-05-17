4aed9c2a175b4956265f79f0812e5ccdb1b99e41
NasriHaithem <mai5haithem@gmail.com>
2022-05-17 13:44:17 +0100

    learning: Lists, Cards, Custom classes, Extracting Widgets, using Function as a parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/quote.dart';
+import 'package:flutter_tutorial/quote_card.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -14,104 +16,37 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  int ninjaLevel = 0;
+  List<Quote> quotes = [
+    Quote(text: "First quote first quote first quote", author: "Haithem") ,
+    Quote(text: "Second quote second quote second quote", author: "Haithem") ,
+    Quote(text: "Third quote third quote third quote", author: "Haithem") ,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent[400],
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-        title: const Text('Ninja Id Card'),
+        title: const Text("Awesome Quotes"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel++;
-          });
-        },
-        backgroundColor: Colors.grey[800],
-        child: const Icon(Icons.add),
-      ),
-      body:  Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/img/cosmos.jpg'),
-                radius: 40.0,
-              ),
-            ),
-
-            Divider(
-              height: 60.0,
-              color: Colors.grey[700],
-            ),
-
-            const Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-            const Text(
-              'Chun-Li',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 30.0,),
-
-            const Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-            Text(
-              '$ninjaLevel',
-              style: const TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 30.0,),
-
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                const SizedBox(width: 10.0,),
-                Text(
-                  "chun-li@gmail.com",
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    letterSpacing: 1.0,
-                    fontSize: 18.0
-                  ),
-                )
-              ],
-            )
-          ],
+          children: quotes.map( (quote) => QuoteCard(
+              quote: quote,
+              delete: () {
+                setState(() {
+                  quotes.remove(quote);
+                });
+              },
+          )).toList(),
         ),
       ),
     );
   }
 }
+
+
---
+++ lib/quote_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote quote;
+  final Function() delete;
+  const QuoteCard(
+      {
+        Key? key,
+        required this.quote,
+        required this.delete
+      }
+  ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+
+              const SizedBox(height: 6.0),
+
+              Text(
+                "- ${quote.author}",
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+
+              TextButton.icon(
+                onPressed: delete,
+                icon: const Icon(Icons.delete),
+                label: const Text("delete quote "),
+                style: TextButton.styleFrom(
+                    primary: Colors.red[700]
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
