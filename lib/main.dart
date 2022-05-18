1614a137bdfe5b6238f30d98bce0304bc8d427ed
NasriHaithem <mai5haithem@gmail.com>
2022-05-18 09:41:58 +0100

    saving

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/choose_location.dart';
+import 'package:flutter_tutorial/pages/home.dart';
+import 'package:flutter_tutorial/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home:Home(),
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
-}
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-        title: const Text('Ninja Id Card'),
-      ),
-      body:  Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/img/cosmos.jpg'),
-                radius: 40.0,
-              ),
-            ),
-
-            Divider(
-              height: 60.0,
-              color: Colors.grey[700],
-            ),
-
-            const Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-            const Text(
-              'Chun-Li',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 30.0,),
-
-            const Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-            const Text(
-              '8',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 30.0,),
-
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                const SizedBox(width: 10.0,),
-                Text(
-                  "chun-li@gmail.com",
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    letterSpacing: 1.0,
-                    fontSize: 18.0
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Map data = {};
+
+  @override
+  Widget build(BuildContext context) {
+    data = ModalRoute.of(context)!.settings.arguments as Map;
+    print(data);
+
+    return Scaffold(
+      body: SafeArea(
+        child: TextButton.icon(
+            onPressed: () {
+              Navigator.pushNamed(context, '/location');
+            },
+            icon: const Icon(Icons.edit_location),
+            label: const Text("update location")
+        ),
+      ),
+    );
+  }
+}
